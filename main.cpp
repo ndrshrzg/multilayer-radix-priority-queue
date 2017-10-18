3592fadd4e40dfe916afc791744e429f80a0112e
ndrshrzg <ndrshrzg@gmail.com>
2017-10-18 13:27:13 +0200

    basic functionality of internal N priority queue added.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <stxxl/vector>
+#include "multilayer_radix_pq.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    N_queue<int, std::string> nq;
+
+    std::cout << "Is empty: " << nq.empty() << std::endl;
+
+    nq.push(3, "three");
+    nq.push(5, "five");
+    nq.push(7, "seven");
+    nq.push(9, "nine");
+
+    std::cout << "Size is: " << nq.size() << std::endl;
+    std::cout << "Minimum is: " << nq.min() << std::endl;
+
+    nq.pop();
+
+    std::cout << "Size is: " << nq.size() << std::endl;
+    std::cout << "Minimum is: " << nq.min() << std::endl;
+
+    std::cout << "Is empty: " << nq.empty() << std::endl;
+
+
+
+
+
+
     return 0;
 }
---
+++ multilayer_radix_pq.h
@@ -0,0 +1,78 @@
+//
+// Created by ndrshrzg on 10/18/17.
+//
+
+#include <stxxl/vector>
+
+
+
+#ifndef MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H_H
+#define MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H_H
+
+
+
+template<typename KeyType, typename ValueType>
+struct CompareInputToPriorityqueue
+{
+public:
+    using key_type = KeyType;
+    using value_type = ValueType;
+    bool operator()(std::pair<int, std::string> l, std::pair<int, std::string> r)
+    {
+        return l.first > r.first;
+    }
+};
+
+template<typename KeyType, typename ValueType>
+class N_queue
+{
+public:
+    using key_type = KeyType;
+    using value_type = ValueType;
+
+private:
+    int size_;
+    //TODO fix instantiation of internal pq, use templated comparator
+    //std::priority_queue<key_type, std::pair<key_type, value_type>, CompareInputToPriorityqueue<key_type, value_type>> q;
+    //std::priority_queue<key_type, std::pair<key_type, value_type>> q;
+    std::priority_queue<key_type> q;
+
+public:
+
+    N_queue() : size_(0){};
+
+    int size() const
+    {
+        return size_;
+    }
+
+    bool empty() const
+    {
+        return (size_ <= 0);
+    }
+
+    void push(key_type key, value_type val)
+    {
+        ++size_;
+        q.push(key);
+    };
+
+    void pop()
+    {
+        q.pop();
+        --size_;
+    }
+
+    key_type min() const
+    {
+        return q.top();
+    }
+
+};
+
+
+
+
+
+
+#endif //MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H_H
