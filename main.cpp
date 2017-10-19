4c9ace76bc89cee4aa7fefa5aeb65f4a50a04414
ndrshrzg <ndrshrzg@gmail.com>
2017-10-19 10:37:49 +0200

    Finished first version of NQueue, ensured monotonicity characteristic of elements, added TODOs

--- main.cpp
+++ main.cpp
@@ -12,14 +12,20 @@ int main() {
     nq.push(5, "five");
     nq.push(7, "seven");
     nq.push(9, "nine");
+    nq.push(8, "eight");
 
     std::cout << "Size is: " << nq.size() << std::endl;
     std::cout << "Minimum is: " << nq.min() << std::endl;
+    std::cout << "Value of Minimum is: " << nq.minVale() << std::endl;
 
     nq.pop();
+    //nq.push(6, "six"); // monotonicity after first pop
+
 
     std::cout << "Size is: " << nq.size() << std::endl;
     std::cout << "Minimum is: " << nq.min() << std::endl;
+    std::cout << "Value of Minimum is: " << nq.minVale() << std::endl;
+
 
     std::cout << "Is empty: " << nq.empty() << std::endl;
 
--- multilayer_radix_pq.h
+++ multilayer_radix_pq.h
@@ -3,25 +3,12 @@
 //
 
 #include <stxxl/vector>
+#include <limits>
 
 
 
 #ifndef MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H_H
 #define MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H_H
-
-
-
-template<typename KeyType, typename ValueType>
-struct CompareInputToPriorityqueue
-{
-public:
-    using key_type = KeyType;
-    using value_type = ValueType;
-    bool operator()(std::pair<int, std::string> l, std::pair<int, std::string> r)
-    {
-        return l.first > r.first;
-    }
-};
 
 template<typename KeyType, typename ValueType>
 class N_queue
@@ -29,13 +16,14 @@ class N_queue
 public:
     using key_type = KeyType;
     using value_type = ValueType;
+    using TPair = std::pair<key_type, value_type>;
+    using VTPairs = std::vector<TPair>;
 
 private:
     int size_;
-    //TODO fix instantiation of internal pq, use templated comparator
-    //std::priority_queue<key_type, std::pair<key_type, value_type>, CompareInputToPriorityqueue<key_type, value_type>> q;
-    //std::priority_queue<key_type, std::pair<key_type, value_type>> q;
-    std::priority_queue<key_type> q;
+    key_type min_ = std::numeric_limits<key_type>::max();
+    //TODO: Clarify whether std::greater<std::pair<K, V>> sorts by first value or by combination? Of latter, write custom Comparator.
+    std::priority_queue<key_type, VTPairs, std::greater<TPair>> q;
 
 public:
 
@@ -50,29 +38,33 @@ public:
     {
         return (size_ <= 0);
     }
-
-    void push(key_type key, value_type val)
+    //TODO: find out if pass by reference is actually more efficient
+    void push(const key_type& key, const value_type& val)
     {
-        //TODO implement last_ minimum functionality to ensure monotonicity
+        assert(key <= min_);
         ++size_;
-        q.push(key);
+        q.push(std::move(std::pair<key_type, value_type>(key, val)));
+
     };
 
     void pop()
     {
+        min_ = q.top().first;
         q.pop();
         --size_;
     }
 
     key_type min() const
     {
-        return q.top();
+        return q.top().first;
     }
+
+    value_type minVale() const {return q.top().second;}
 
 };
 
 
-
+//TODO: Datastructure buckets, enclosing datastructure multilayer_radix_pq using Nqueue and buckets
 
 
 
