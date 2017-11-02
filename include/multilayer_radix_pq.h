aeb6e66f2226bcd67acd762edac9a5fc3b3e8c88
ndrshrzg <ndrshrzg@gmail.com>
2017-11-02 21:25:10 +0100

    added calculateBucket method that calculates the according bucket and block to a given key. Added TODOS, removed deprecated methods

--- include/multilayer_radix_pq.h
+++ include/multilayer_radix_pq.h
@@ -21,62 +21,53 @@
 ///  will fit in internal memory
 
 
-template<typename KeyType, typename ValueType, size_t radix>
-class multilayer_radix_pq
-{
-public:
-    using key_type = KeyType;
-    using value_type = ValueType;
-    using block_type = stxxl::queue<std::pair<key_type, value_type>>;
-    using buckets_type = std::array<block_type, radix>;
+namespace multilayer_radix_pq {
 
-private:
+    namespace internal {
+        static constexpr std::pair<size_t, size_t> calculateBucket(uint64_t key, uint64_t last, uint64_t r)
+        {
+            //Left very explicit for now to debug
+            size_t index_least_significant = (std::numeric_limits<uint64_t>::digits - __builtin_clzll(key ^ last)) - 1;
+            size_t i = static_cast<size_t>(ceil(index_least_significant / r));
+            size_t shift = r*i;
+            size_t j = (key >> shift+1);
+            // Index is limited to 16 at the moment since instantiating more stxxl::queues crashes the system
+            assert(j <= 16);
+            return std::pair<size_t, size_t>(i,j);
+        };
+    }; // end of namespace internal
 
-    static const int len = std::numeric_limits<key_type>::digits;
 
-    std::array<buckets_type, len> buckets_;
-    std::array<bool, len> bucket_flags_;
+    template<typename KeyType, typename ValueType>//, size_t radix>
+    class multilayer_radix_pq {
+    public:
+        using key_type = KeyType;
+        using value_type = ValueType;
+        using block_type = stxxl::queue<std::pair<key_type, value_type>>;
+    private:
+        int r_, C_, last_;
+        static const int len = std::numeric_limits<key_type>::digits;
+        std::array<std::array<block_type, 16>, len> buckets_;
+        std::array<bool, len> bucket_flags_;
 
-public:
-    multilayer_radix_pq()
-    {
-        bucket_flags_.fill(0);
+    public:
+        multilayer_radix_pq(int r, int C) : r_(r), C_(C), last_(), buckets_() {
+            bucket_flags_.fill(0);
+        };
+
+        void push(key_type key, value_type val) {
+            std::pair<uint64_t, uint64_t> pos = internal::calculateBucket(key, last_, r_);
+            bucket_flags_[pos.first] = 1;
+            std::cout << "Pushing into B(" << pos.first << ", " << pos.second << ")." << std::endl;
+            buckets_[pos.first].at(pos.second).push(std::pair<key_type, value_type>(key, val));
+        }
+
+        bool empty() {
+            return buckets_[0].empty();
+        }
+
     };
-
-    void push(key_type key, value_type val)
-    {
-        //TODO calculate bucket, [i][j] instead of [i][0] with i being index of least significant bit changed and j being key at index i
-        std::pair<int, int> pos = calculateBucket(key);
-        bucket_flags_[pos.first] = 1;
-        buckets_[pos.first][pos.second].push(std::pair<key_type, value_type>(key, val));
-    }
-    bool empty()
-    {
-        return buckets_[0].empty();
-    }
-
-private:
-    std::pair<int, int> calculateBucket(key_type key)
-    {
-        //TODO calculate key in base r_
-        // find least significant bit different from min in base r, return index of lsb and value as pair
-        // so res.first is bucket index, res.second is block index
-        int i = key % len; // mock calculations
-        int j = key % radix;
-        return std::pair<int, int>(i,j);
-    }
-
-
-    int changeBase(key_type key, size_t r)
-    {
-        std::cout << "key: " << key << "base: " << r << std::endl;
-        return key;
-    }
-
-
-};
-
-
+}
 
 
 #endif //MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "include/multilayer_radix_pq.h"
 
-int main() {
+typedef struct data
+{
+    int x;
+} DATA;
 
-    multilayer_radix_pq<uint64_t, std::string, 8> mlrq;
-    mlrq.push(1, "test");
-    mlrq.push(53, "test");
-    mlrq.push(871, "test");
+int main() {
+    data x;
+    //multilayer_radix_pq<uint64_t, std::string, 8> mlrq;
+    multilayer_radix_pq::multilayer_radix_pq<uint64_t, DATA> mlrq(8, 4096);
+    mlrq.push(pow(2,17)+pow(2,10)+pow(2,3), x);
+    mlrq.push(11, x);
+    mlrq.push(11, x);
+    // Calls resulting in Blocks > 16 not allowed for now
+    //mlrq.push(pow(2,pow(2,16)), x);
     std::cout << "Is empty: " << mlrq.empty() << std::endl;
     return 0;
 }
