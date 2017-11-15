a7de9957ca2776812ce7f114662c64b606043e49
ndrshrzg <ndrshrzg@gmail.com>
2017-11-15 13:48:08 +0100

    started pop method, updated todos, REVIEW NEEDED FOR CALCULATEBUCKET

--- include/multilayer_radix_pq.h
+++ include/multilayer_radix_pq.h
@@ -8,11 +8,11 @@
 #include <cmath>
 #include <limits>
 #include <iterator>
+#include <cassert>
 
 #ifndef MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H
 #define MULTILAYER_RADIX_PRIORITY_QUEUE_MULTILAYER_RADIX_PQ_H
 
-//TODO: Datastructure buckets, enclosing datastructure multilayer_radix_pq using Nqueue and buckets
 /// Outline:
 ///  Bitvector of size h (limited by C in base r?) indicating whether bucket is empty
 ///  Buckets (std::array) of size r pointing to
@@ -25,9 +25,11 @@ namespace multilayer_radix_pq {
 
     namespace internal {
         static constexpr auto calculateBucket(uint64_t key, uint64_t last, size_t r) {
+            // TODO REVIEW IF THIS IF WORKING CORRECTLY, WHY ARE 128 and 129 PUSHED INTO SAME BUCKET???
             //Left very explicit for now to debug
+            auto x = __builtin_clzll(key ^ last);
             size_t index_least_significant = (std::numeric_limits<uint64_t>::digits - __builtin_clzll(key ^ last)) - 1;
-            auto i = static_cast<size_t>(floor(index_least_significant / log2(r)));
+            size_t i = floor(index_least_significant / log2(r));
             auto shift = static_cast<size_t>(log2(r) * i);
             size_t j = (key >> shift);
             return std::pair<size_t, size_t>(i, j);
@@ -52,7 +54,7 @@ namespace multilayer_radix_pq {
         static const size_t radix = size_t(1) << RADIX_BITS;
         static const size_t no_of_queues = static_cast<size_t>(radix * ceil(len / log2(radix)));
         int C_;
-        uint64_t last_minimum_;
+        key_type last_minimum_;
         std::array<std::array<block_type, no_of_queues>, len> buckets_;
         std::array<std::pair<int, std::array<bool, no_of_queues>>, len> bucket_empty_flags_;
 
@@ -66,18 +68,41 @@ namespace multilayer_radix_pq {
             // TODO replace reinterpret_cast<>(key) with encoder
             // TODO special case of N bucket
             // TODO implement last
+            assert(key >= last_minimum_);
             std::pair<uint64_t, uint64_t> pos = internal::calculateBucket(
                     reinterpret_cast<uint64_t>(key), last_minimum_, radix);
+            /// IDEA: set empty_flag to += 1 to simultaneously count the number of elemens inside? this way
+            /// the count of elemens can be decremented when poping an element
             bucket_empty_flags_[pos.first].first = bucket_empty_flags_[pos.first].second[pos.second] = 1;
             std::cout << "Pushing into B(" << pos.first << ", " << pos.second << ")." << std::endl;
-            buckets_[pos.first].at(pos.second).push_back(std::pair<key_type, value_type>(key, val));
+            buckets_[pos.first][pos.second].push_back(std::pair<key_type, value_type>(key, val));
         }
 
-        void pop() {
+        std::pair<key_type, value_type> pop() {
             //TODO get elem from first non empty bucket, update last_ and reorganize elems in bucket
             /// use find_first_non_empty then pull data -> remove element, if bucket empty update min and
             /// reorganize
-            return;
+            // find position of first non empty bucket
+            std::pair<int, int> pos_minimum_element_ = top();
+            // access element at first non empty bucket position
+            std::pair<key_type, value_type> minimum_element_ = buckets_[pos_minimum_element_.first][pos_minimum_element_.second].back();
+            // update bucket empty flags if vectors were emptied
+            if (buckets_[pos_minimum_element_.first][pos_minimum_element_.second].empty())
+                bucket_empty_flags_[pos_minimum_element_.first].second[pos_minimum_element_.second] = 0;
+            for (int j = 0; j < no_of_queues; j++){
+                if (buckets_[pos_minimum_element_.first][j].empty())
+                {
+                    bucket_empty_flags_[pos_minimum_element_.first].first = 0;
+                    // TODO at this point reorganization needs to take place
+                }
+
+            }
+            // pop element from first non empty bucket
+            buckets_[pos_minimum_element_.first][pos_minimum_element_.second].pop_back();
+            //update last_minimum_
+            last_minimum_ = minimum_element_.first;
+
+            return minimum_element_;
         }
 
         auto top() {
--- main.cpp
+++ main.cpp
@@ -9,11 +9,8 @@ typedef struct data
 int main() {
     data x;
     multilayer_radix_pq::multilayer_radix_pq<uint64_t, DATA, 3> mlrpq(4096);
-    mlrpq.push(8, x);
-    mlrpq.push(pow(2,7), x);
-    mlrpq.push(9087, x);
-    mlrpq.push(pow(2,11) + pow(2,9), x);
-    std::cout << "Is empty: " << mlrpq.empty() << std::endl;
-    std::cout << "First non empty Bucket: " << mlrpq.top().first << ", " << mlrpq.top().second << std::endl;
+    mlrpq.push(128, x);
+    mlrpq.push(129, x);
+
     return 0;
 }
