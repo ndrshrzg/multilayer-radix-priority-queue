e101e8d867387fe50a6efdef4e45b56ac43c7e1f
ndrshrzg <ndrshrzg@gmail.com>
2017-11-16 16:10:57 +0100

    implemented pop() method with reorganization of buckets, added scan method to find minimum in bucket (necessary for reorganization), extracted method for updateBucketEmptyFlags

--- include/multilayer_radix_pq.h
+++ include/multilayer_radix_pq.h
@@ -25,20 +25,35 @@ namespace multilayer_radix_pq {
 
     namespace internal {
         static constexpr auto calculateBucket(uint64_t key, uint64_t last, size_t r) {
-            // TODO REVIEW IF THIS IF WORKING CORRECTLY, WHY ARE 128 and 129 PUSHED INTO SAME BUCKET???
             //Left very explicit for now to debug
-            auto x = __builtin_clzll(key ^ last);
-            size_t index_least_significant = (std::numeric_limits<uint64_t>::digits - __builtin_clzll(key ^ last)) - 1;
-            size_t i = floor(index_least_significant / log2(r));
-            auto shift = static_cast<size_t>(log2(r) * i);
-            size_t j = (key >> shift);
-            return std::pair<size_t, size_t>(i, j);
+            //TODO : needs review, if last = 128, push(130) pushes into B(0, 130) instead of B(0, 2)
+            if(key == last) {
+                return std::pair<size_t, size_t>(0, 0);
+            }
+            else{
+                size_t index_highest_significant = (std::numeric_limits<uint64_t>::digits - __builtin_clzll(key ^ last)) - 1;
+                size_t i = floor(index_highest_significant / log2(r));
+                auto shift = static_cast<size_t>(log2(r) * i);
+                size_t j = (key >> shift);
+                return std::pair<size_t, size_t>(i, j);
+            }
         };
+
+        template<typename key_type, typename block_type>
+        static constexpr key_type scanBucketForMinimum(block_type bucket){
+            key_type reorganization_minimum = std::numeric_limits<key_type>::max();
+            while (!(bucket.empty())){
+                if (bucket.back().first < reorganization_minimum){
+                    reorganization_minimum = bucket.back().first;
+                }
+                bucket.pop_back();
+            }
+            return reorganization_minimum;
+        }
 
         static constexpr auto findFirstNonEmpty(/*bucket_empty_flags_*/){};
 
     }; // end of namespace internal
-
 
     template<typename KeyType, typename ValueType, size_t RADIX_BITS>
     class multilayer_radix_pq {
@@ -59,7 +74,7 @@ namespace multilayer_radix_pq {
         std::array<std::pair<int, std::array<bool, no_of_queues>>, len> bucket_empty_flags_;
 
     public:
-        explicit multilayer_radix_pq(int C) : C_(C) {//, array_last_(){//}, buckets_() {
+        explicit multilayer_radix_pq(int C) : C_(C) {
             bucket_empty_flags_ = {};
             last_minimum_ = std::numeric_limits<key_type>::min();
         };
@@ -67,42 +82,68 @@ namespace multilayer_radix_pq {
         void push(key_type key, value_type val) {
             // TODO replace reinterpret_cast<>(key) with encoder
             // TODO special case of N bucket
-            // TODO implement last
             assert(key >= last_minimum_);
             std::pair<uint64_t, uint64_t> pos = internal::calculateBucket(
                     reinterpret_cast<uint64_t>(key), last_minimum_, radix);
-            /// IDEA: set empty_flag to += 1 to simultaneously count the number of elemens inside? this way
-            /// the count of elemens can be decremented when poping an element
             bucket_empty_flags_[pos.first].first = bucket_empty_flags_[pos.first].second[pos.second] = 1;
-            std::cout << "Pushing into B(" << pos.first << ", " << pos.second << ")." << std::endl;
+            std::cout << "Pushing into B(" << pos.first << ", " << pos.second << ")." << std::endl; // output
             buckets_[pos.first][pos.second].push_back(std::pair<key_type, value_type>(key, val));
         }
 
         std::pair<key_type, value_type> pop() {
-            //TODO get elem from first non empty bucket, update last_ and reorganize elems in bucket
-            /// use find_first_non_empty then pull data -> remove element, if bucket empty update min and
-            /// reorganize
             // find position of first non empty bucket
             std::pair<int, int> pos_minimum_element_ = top();
-            // access element at first non empty bucket position
-            std::pair<key_type, value_type> minimum_element_ = buckets_[pos_minimum_element_.first][pos_minimum_element_.second].back();
-            // update bucket empty flags if vectors were emptied
-            if (buckets_[pos_minimum_element_.first][pos_minimum_element_.second].empty())
-                bucket_empty_flags_[pos_minimum_element_.first].second[pos_minimum_element_.second] = 0;
-            for (int j = 0; j < no_of_queues; j++){
-                if (buckets_[pos_minimum_element_.first][j].empty())
-                {
-                    bucket_empty_flags_[pos_minimum_element_.first].first = 0;
-                    // TODO at this point reorganization needs to take place
+            // if B(0, m0) empty
+            if (pos_minimum_element_.first != 0) {
+                std::cout << "begin reorganization phase" << std::endl;
+                // calculate minimum of current first non empty bucket
+                key_type reorganization_minimum = internal::scanBucketForMinimum<key_type, block_type>(buckets_[pos_minimum_element_.first][pos_minimum_element_.second]);
+                // assign new minimum
+                last_minimum_ = reorganization_minimum;
+                // reorganize bucket into B(0, m0) using reorganization_minimum
+                while(!buckets_[pos_minimum_element_.first][pos_minimum_element_.second].empty()){
+                    // retrieve arbitrary element from current bucket
+                    std::pair<key_type, value_type> temp_element = buckets_[pos_minimum_element_.first][pos_minimum_element_.second].back();
+                    // push arbitrary element in mlrpq using the calculated minimum
+                    push(temp_element.first, temp_element.second);
+                    // remove element from old bucket
+                    buckets_[pos_minimum_element_.first][pos_minimum_element_.second].pop_back();
                 }
-
+                // update flags for _old_ bucket
+                updateBucketEmptyFlags(pos_minimum_element_);
+                std::cout << "end reorganization phase" << std::endl;
+                // call pop again after reorganization
+                pop();
             }
-            // pop element from first non empty bucket
-            buckets_[pos_minimum_element_.first][pos_minimum_element_.second].pop_back();
-            //update last_minimum_
-            last_minimum_ = minimum_element_.first;
+            else{
+                // retrieve element from bucket
+                std::pair<key_type, value_type> minimum_element_ =
+                        buckets_[pos_minimum_element_.first][pos_minimum_element_.second].back();
+                // pop element from bucket
+                buckets_[pos_minimum_element_.first][pos_minimum_element_.second].pop_back();
+                // print popped element to console
+                std::cout << "Popped: (" << minimum_element_.first << ", " << minimum_element_.second.x << ")."
+                          << std::endl;
+                // check bucket empty flags and update if necessary
+                updateBucketEmptyFlags(pos_minimum_element_);
+                // return minimum element
+                return minimum_element_;
+            }
+        }
 
-            return minimum_element_;
+        // update bucket empty flags if vectors were emptied
+        void updateBucketEmptyFlags(const std::pair<int, int> pos_update) {
+            // check if bucket is empty
+            if (buckets_[pos_update.first][pos_update.second].empty()){
+                bucket_empty_flags_[pos_update.first].second[pos_update.second] = 0;
+                // if bucket became empty, check if whole array is empty
+                int temp = 0;
+                for (int j = 0; j < no_of_queues; j++){
+                    if (buckets_[pos_update.first][j].empty()){ temp += 1; }
+                }
+                if(temp == no_of_queues) { bucket_empty_flags_[pos_update.first].first = 0;
+                    std::cout << "Array " << pos_update.first << " empty" << std::endl;} // output
+            }
         }
 
         auto top() {
@@ -123,6 +164,7 @@ namespace multilayer_radix_pq {
             return (top() == std::pair<int,int>(-1, -1));
         }
 
+        //this method only exists for a unit test which is weird
         bool bucketEmpty(int k){
             return !bucket_empty_flags_[k].first;
         }
--- main.cpp
+++ main.cpp
@@ -9,8 +9,19 @@ typedef struct data
 int main() {
     data x;
     multilayer_radix_pq::multilayer_radix_pq<uint64_t, DATA, 3> mlrpq(4096);
+    mlrpq.push(7, x);
+    std::cout << "Queue empty: " << mlrpq.empty() << std::endl;
+    mlrpq.push(8, x);
+    mlrpq.push(pow(2,7), x);
     mlrpq.push(128, x);
-    mlrpq.push(129, x);
-
+    mlrpq.push(pow(2, 11), x);
+    mlrpq.pop();
+    mlrpq.pop();
+    mlrpq.pop();
+    mlrpq.pop();
+    mlrpq.push(pow(2, 13), x);
+    mlrpq.pop();
+    mlrpq.pop();
+    std::cout << "Queue empty: " << mlrpq.empty() << std::endl;
     return 0;
 }
